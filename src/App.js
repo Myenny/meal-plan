bf11c438f52f897b81a46eff4041b03225bef90a
ooeygooeygold <amichaelj1@gmail.com>
2019-02-17 01:38:10 -0500

    Dynamically pulling recipe info from static state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react'
 import blueberry from './assets/blueberry.jpg'
 import './App.css'
+import axios from 'axios'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      recipeName: 'wiener wiener Chicken Dinner',
+      recipeTime: '25 hours',
+      recipeIngredients: 'inged'
+    }
+  }
   render() {
     return (
       <div>
-        <ul>Meal Plan</ul>
+        <h1>Meal Plan</h1>
 
         <div>
           <img src={blueberry} />
+        </div>
+        <div>
+          <h2>{this.state.recipeName}</h2>
+          <h2>{this.state.recipeTime}</h2>
+          <h3>{this.state.recipeIngredients}</h3>
         </div>
       </div>
     )
